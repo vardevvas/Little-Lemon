acd2be69f059cc9eddae29d7ab57443f319ccff6
Ali AwadAlkarim <vardevvas@gmail.com>
2023-09-11 06:50:16 +0400

    added the mainview

--- Little Lemon/MenuItemsView.swift
+++ Little Lemon/MenuItemsView.swift
@@ -7,9 +7,71 @@
 
 import SwiftUI
 
-struct MenuItemsView: View {
+struct GridItemView: View {
+    let imageName: String
+    let title: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+                .padding(8)
+            
+            Text(title)
+                .font(.headline)
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.blue)
+        .cornerRadius(8)
+    }
+}
+
+
+struct MenuItemsView: View {
+
+    
+    let columns: [GridItem] = [
+            GridItem(.flexible(), spacing: 16),
+            GridItem(.flexible(), spacing: 16),
+            GridItem(.flexible(), spacing: 16)
+        ]
+
+    var body: some View {
+        List{
+            Section(header: Text("Food")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHGrid(rows: columns, spacing: 16) {
+                        ForEach(0..<5) { index in
+                            GridItemView(imageName: "photo", title: "Item \(index + 1)")
+                        }
+                    }
+                    .padding(16)
+                }
+            }
+            Section(header: Text("Drinks")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHGrid(rows: columns, spacing: 16) {
+                        ForEach(0..<5) { index in
+                            GridItemView(imageName: "photo", title: "Item \(index + 1)")
+                        }
+                    }
+                    .padding(16)
+                }
+            }
+            Section(header: Text("Desert")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHGrid(rows: columns, spacing: 16) {
+                        ForEach(0..<5) { index in
+                            GridItemView(imageName: "photo", title: "Item \(index + 1)")
+                        }
+                    }
+                    .padding(16)
+                }
+            }
+
+        }
     }
 }
 
