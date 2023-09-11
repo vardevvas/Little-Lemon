ddfc8f5d57c1cd4a63dc7f4816a1ace433f242ab
Ali AwadAlkarim <vardevvas@gmail.com>
2023-09-11 05:59:24 +0400

    Initial Commit

---
+++ Little Lemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by Ali AwadAlkarim on 11/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
