6ef9ec1c14ebf8dde628c4ac63180f85aace4d2c
Ali AwadAlkarim <vardevvas@gmail.com>
2023-09-11 06:49:32 +0400

    added mainview

---
+++ Little Lemon/MenuItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemsView.swift
+//  Little Lemon
+//
+//  Created by Ali AwadAlkarim on 11/09/2023.
+//
+
+import SwiftUI
+
+struct MenuItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuItemsView()
+}
